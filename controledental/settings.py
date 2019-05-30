31e32199e7b083cb96111720d1a0fabda1a9d6e3
João garcia <joaogarciadelimaneto@gmail.com>
2019-05-30 08:26:28 -0300

    Configurado colectstatic e Amazon s3
    close #30

--- controledental/settings.py
+++ controledental/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'controledental.base'
 ]
@@ -131,6 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MERIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
@@ -142,6 +145,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
+    COLLECTFAST_ENABLED = False
     AWS_DEFAULT_ACL = 'private'
     AWS_S3_CUSTOM_DOMAIN = None
 
