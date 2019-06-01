be34af4a4aa5c06e70c9c21b67783751cce884b5
João garcia <joaogarciadelimaneto@gmail.com>
2019-06-01 08:38:17 -0300

    configurado primeiro teste de conteudo

--- controledental/base/tests/test_home.py
+++ controledental/base/tests/test_home.py
@@ -1,6 +1,12 @@
 from django.test import Client
 
+from controledental.django_assertions import assert_contains
+
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Controle dental!</title>')
